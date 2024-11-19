037f15723201bfd156abf3bb72919d0e1fd97958
Trek119 <kopen_kin@mail.ru>
2024-11-19 20:22:16 +0300

    new metod main

--- src/main/java/Alex119/Main.java
+++ src/main/java/Alex119/Main.java
@@ -2,6 +2,12 @@ package Alex119;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Worl123d!");
+        int a = 10;
+        for (int i=0;a>i;i++)
+            if (i==5){
+                System.out.println("Пять");
+            } else{System.out.println(i);
+            }
+
     }
 }
