0559390fa8484aa3b6dec8f4123a303fb66ebaea
Trek119 <kopen_kin@mail.ru>
2024-11-19 20:34:23 +0300

    новые условия для else

--- src/main/java/Alex119/Main.java
+++ src/main/java/Alex119/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         for (int i=0;a>i;i++)
-            if (i==5){
+            if (i==5||i>5){
                 System.out.println("Пять");
-            } else{System.out.println(i);
+            } else{System.out.println(a);
             }
 
     }
