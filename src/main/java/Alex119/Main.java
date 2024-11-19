72f1f5ecb2f022a1e776f33063f2a15144b0f38b
Alexsantr <50623230+Alexsantr@users.noreply.github.com>
2024-11-19 20:09:07 +0300

    Update Main.java

--- src/main/java/Alex119/Main.java
+++ src/main/java/Alex119/Main.java
@@ -2,6 +2,6 @@ package Alex119;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Worl111111d!");
     }
 }
